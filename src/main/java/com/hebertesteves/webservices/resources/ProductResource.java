0541baa929d51a3339c9299a9111f81cc09931a2
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-10-18 12:56:12 -0300

    Product

---
+++ src/main/java/com/hebertesteves/webservices/resources/ProductResource.java
@@ -0,0 +1,32 @@
+package com.hebertesteves.webservices.resources;
+
+import com.hebertesteves.webservices.entities.Product;
+import com.hebertesteves.webservices.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductResource {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll() {
+        List<Product> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> findById(@PathVariable Long id) {
+        Product product = service.findById(id);
+        return ResponseEntity.ok().body(product);
+    }
+}
