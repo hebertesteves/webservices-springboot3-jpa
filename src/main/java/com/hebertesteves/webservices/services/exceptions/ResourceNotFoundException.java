dc2eeb2c3666dbbc64b663d005d3a941573e9ad6
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-10-23 01:04:00 -0300

    Exception handling - findById

---
+++ src/main/java/com/hebertesteves/webservices/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.hebertesteves.webservices.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
