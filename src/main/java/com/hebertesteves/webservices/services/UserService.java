3323d4a84246c8e281e782a62356d299bd9f979e
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-10-23 15:55:59 -0300

    Exception handling - update

--- src/main/java/com/hebertesteves/webservices/services/UserService.java
+++ src/main/java/com/hebertesteves/webservices/services/UserService.java
@@ -4,6 +4,7 @@ import com.hebertesteves.webservices.entities.User;
 import com.hebertesteves.webservices.repositories.UserRepository;
 import com.hebertesteves.webservices.services.exceptions.DatabaseException;
 import com.hebertesteves.webservices.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -45,9 +46,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
